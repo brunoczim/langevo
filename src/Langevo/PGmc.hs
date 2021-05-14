601b58071842529f35de58262082f4433cd65b8b
Bruno Zimmermann <brunoczim@gmail.com>
2021-05-14 10:41:27 -0300

    more sound laws

--- src/Langevo/PGmc.hs
+++ src/Langevo/PGmc.hs
@@ -36,8 +36,9 @@ module Langevo.PGmc
   , phonemeW
   ) where
 
+import Data.Maybe (fromMaybe)
 import qualified Langevo.PIE as PIE
-import Data.Foldable (foldr')
+import Data.Foldable (foldl')
 import Data.Text (Text)
 import Prelude hiding (Word)
 
@@ -45,18 +46,97 @@ type Phoneme = Text
 type Word = [Phoneme]
 
 fromPIE :: PIE.Word -> Word
-fromPIE text = foldr' ($) (mconcat text) shifts
+fromPIE word = mconcat (foldl' (flip ($)) word shifts)
   where
-    shifts :: [Word -> Word]
-    shifts = [centumShift]
-    centumShift :: Word -> Word
+    shifts :: [PIE.Word -> PIE.Word]
+    shifts =
+      [ centumShift
+      , sonEpenthesis
+      , dentalEpenthesis
+      , consonShortening
+      , overlonging
+      ]
+    centumShift :: PIE.Word -> PIE.Word
     centumShift =
       let shift ph
             | ph == PIE.phonemeKj = PIE.phonemeK
             | ph == PIE.phonemeGj = PIE.phonemeG
             | ph == PIE.phonemeGjh = PIE.phonemeGh
             | otherwise = ph
+      in fmap (fmap shift)
+    sonEpenthesis :: PIE.Word -> PIE.Word
+    sonEpenthesis =
+      let shift morpheme = do
+            ph <- morpheme
+            let answer
+                  | ph == PIE.phonemeMSyl = [PIE.phonemeU, PIE.phonemeM]
+                  | ph == PIE.phonemeMSylAcc = [PIE.phonemeUAcc, PIE.phonemeM]
+                  | ph == PIE.phonemeNSyl = [PIE.phonemeU, PIE.phonemeN]
+                  | ph == PIE.phonemeNSylAcc = [PIE.phonemeUAcc, PIE.phonemeN]
+                  | ph == PIE.phonemeRSyl = [PIE.phonemeU, PIE.phonemeR]
+                  | ph == PIE.phonemeRSylAcc = [PIE.phonemeUAcc, PIE.phonemeR]
+                  | ph == PIE.phonemeLSyl = [PIE.phonemeU, PIE.phonemeL]
+                  | ph == PIE.phonemeLSylAcc = [PIE.phonemeUAcc, PIE.phonemeL]
+                  | otherwise = [ph]
+            answer
       in fmap shift
+    dentalEpenthesis :: PIE.Word -> PIE.Word
+    dentalEpenthesis [last] = [last]
+    dentalEpenthesis (curr : (right : suffix) : tail)
+      | isDental left && isDental right = currAnswer : answerTail
+      | otherwise = curr : dentalEpenthesis ((right : suffix) : tail)
+      where
+        isDental = (`elem` [PIE.phonemeD, PIE.phonemeT])
+        prefix = init curr
+        left = last curr
+        currAnswer = prefix ++ [PIE.phonemeS]
+        answerTail = dentalEpenthesis ((PIE.phonemeS : suffix) : tail)
+    consonShortening :: PIE.Word -> PIE.Word
+    consonShortening word =
+      let shortVowels =
+            [ PIE.phonemeA, PIE.phonemeAAcc
+            , PIE.phonemeE, PIE.phonemeEAcc
+            , PIE.phonemeI, PIE.phonemeIAcc
+            , PIE.phonemeO, PIE.phonemeOAcc
+            , PIE.phonemeU, PIE.phonemeUAcc
+            ]
+          iterMorphemes _ _ [] = []
+          iterMorphemes prev gem (head : tail) =
+            let (prev', gem', head') = iterMorpheme prev gem head
+            in head' : iterMorphemes prev' gem' tail
+          iterMorpheme prev gem [] = (prev, gem, [])
+          iterMorpheme prev gem (curr : tail)
+            | Just curr == gem && prevShortens = (prev', gem', rest)
+            | otherwise = (prev', gem', curr : rest)
+            where
+              prevShortens = maybe True (`notElem` shortVowels) prev
+              (prev', gem', rest) = iterMorpheme gem (Just curr) tail
+        in iterMorphemes Nothing Nothing word
+    overlonging :: PIE.Word -> PIE.Word
+    overlonging [] = []
+    overlonging word =
+      let longVowels =
+            [ PIE.phonemeAa, PIE.phonemeAaAcc
+            , PIE.phonemeEe, PIE.phonemeEeAcc
+            , PIE.phonemeIi, PIE.phonemeIiAcc
+            , PIE.phonemeOo, PIE.phonemeOoAcc
+            , PIE.phonemeUu, PIE.phonemeUuAcc
+            ]
+          initMorphemes = init word
+          lastMorpheme = last word
+          initPhonemes = init lastMorpheme
+          lastPhoneme = last lastMorpheme
+          overlongLast
+            | lastPhoneme == PIE.phonemeAa = "â"
+            | lastPhoneme == PIE.phonemeEe = phonemeEe
+            | lastPhoneme == PIE.phonemeIi = "î"
+            | lastPhoneme == PIE.phonemeOo = phonemeOo
+            | lastPhoneme == PIE.phonemeUu = "û"
+          word'
+            | null lastMorpheme || lastPhoneme `notElem` longVowels = word
+            | otherwise = initMorphemes ++ [initPhonemes ++ [overlongLast]]
+      in word'
+          
 
 phonemeA :: Phoneme
 phonemeA = "a"
